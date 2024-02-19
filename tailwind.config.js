b7257177025b8266439b08f408013da7d19fa584
imrashidov <farikrashidov@gmail.com>
2024-02-20 03:20:56 +0400

    Add: Background Image

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        "background-move": "translate 0 300px",
+      },
+    },
   },
   plugins: [],
 };
