a6b2eba2eb375238e5ee64af70a1325e446e4e77
imrashidov <farikrashidov@gmail.com>
2024-02-26 20:01:58 +0400

    Add: Password show or hide button

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,7 +1,15 @@
+import { useState } from "react";
 import { FaLock } from "react-icons/fa6";
 import { FaUser } from "react-icons/fa";
+import { FaRegEye } from "react-icons/fa";
+import { FaRegEyeSlash } from "react-icons/fa";
 
 const Login = () => {
+  const [showPassword, setShowPassword] = useState(false);
+  const handleShowPassword = (e) => {
+    e.preventDefault();
+    setShowPassword(!showPassword);
+  };
   return (
     <div className="background">
       <div className="login">
@@ -24,9 +32,12 @@ const Login = () => {
                 <FaLock className="login-icon" />
                 <input
                   placeholder="Password"
-                  type="password"
-                  className="login-input"
+                  type={showPassword ? "text" : "password"}
+                  className="login-input relative"
                 />
+                <button onClick={handleShowPassword} className="password-icon">
+                  {showPassword ? <FaRegEyeSlash /> : <FaRegEye />}
+                </button>
               </div>
               <div className="login-buttons">
                 <button className="btn-login">Login</button>
